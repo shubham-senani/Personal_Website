030e7f4482605944956e9f69e55338255a9fa3f1
skipperr09 <shubhamsenani9@gmail.com>
2023-04-03 12:45:01 +0530

    update changes

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -32,10 +32,10 @@ const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("light");
+    document.body.classList.toggle("dark");
 })
 window.addEventListener("load", () => {
-    if(document.body.classList.contains("light"))
+    if(document.body.classList.contains("dark"))
     {
         dayNight.querySelector("i").classList.add("fa-sun");
     }
